ba61d6c03cf5de0d7af1d04149b29f0c435ceb20
popkidxtech <popkidbotdeployment@gmail.com>
2025-03-21 00:22:13 +0300

    𝚈

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function downloadSessionData() {
         console.error('Please add your session to SESSION_ID env !!');
         return false;
     }
-    const sessdata = config.SESSION_ID.split("popkidmd$")[1];
+    const sessdata = config.SESSION_ID.split("Popkidmd$")[1];
     const url = `https://pastebin.com/raw/${sessdata}`;
     try {
         const response = await axios.get(url);
