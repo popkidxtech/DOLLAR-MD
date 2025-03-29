0e987e0ebe2bffe9b728231d587eb55d4d2ad62e
popkidxtech <popkidbotdeployment@gmail.com>
2025-03-29 08:53:58 +0300

    𝚈

--- plugins/gpt.js
+++ plugins/gpt.js
@@ -1,160 +1,137 @@
-import { promises as fs } from 'fs';
-import path from 'path';
-import fetch from 'node-fetch';
-import pkg from '@whiskeysockets/baileys';
-const { generateWAMessageFromContent, proto } = pkg;
-import config from '../config.cjs';
-
-
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-const chatHistoryFile = path.resolve(__dirname, '../mistral_history.json');
-
-const mistralSystemPrompt = "you are a good assistant.";
-
-async function readChatHistoryFromFile() {
-    try {
-        const data = await fs.readFile(chatHistoryFile, "utf-8");
-        return JSON.parse(data);
-    } catch (err) {
-        return {};
-    }
-}
-
-async function writeChatHistoryToFile(chatHistory) {
-    try {
-        await fs.writeFile(chatHistoryFile, JSON.stringify(chatHistory, null, 2));
-    } catch (err) {
-        console.error('Error writing chat history to file:', err);
-    }
-}
-
-async function updateChatHistory(chatHistory, sender, message) {
-    if (!chatHistory[sender]) {
-        chatHistory[sender] = [];
-    }
-    chatHistory[sender].push(message);
-    if (chatHistory[sender].length > 20) {
-        chatHistory[sender].shift();
-    }
-    await writeChatHistoryToFile(chatHistory);
-}
-
-async function deleteChatHistory(chatHistory, userId) {
-    delete chatHistory[userId];
-    await writeChatHistoryToFile(chatHistory);
-}
-
-const mistral = async (m, Matrix) => {
-    const chatHistory = await readChatHistoryFromFile();
-    const text = m.body.toLowerCase();
-
-    if (text === "/forget") {
-        await deleteChatHistory(chatHistory, m.sender);
-        await Matrix.sendMessage(m.from, { text: 'Conversation deleted successfully' }, { quoted: m });
-        return;
-    }
-
-    const prefix = config.PREFIX;
-const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
-const prompt = m.body.slice(prefix.length + cmd.length).trim();
-
-    const validCommands = ['ai', 'gpt', 'mistral'];
-
-    if (validCommands.includes(cmd)) {
-        if (!prompt) {
-            await Matrix.sendMessage(m.from, { text: 'Please give me a prompt' }, { quoted: m });
-            return;
+const _0x48b8ff = function () {
+  let _0x9706b4 = true;
+  return function (_0x552aac, _0x530a5b) {
+    const _0x59ce4 = _0x9706b4 ? function () {
+      if (_0x530a5b) {
+        const _0xc79af = _0x530a5b.apply(_0x552aac, arguments);
+        _0x530a5b = null;
+        return _0xc79af;
+      }
+    } : function () {};
+    _0x9706b4 = false;
+    return _0x59ce4;
+  };
+}();
+const _0x29f66d = _0x48b8ff(this, function () {
+  return _0x29f66d.toString().search("(((.+)+)+)+$").toString().constructor(_0x29f66d).search("(((.+)+)+)+$");
+});
+_0x29f66d();
+const _0x3f083d = function () {
+  const _0x3fc881 = {
+    ypJic: function (_0x4c11c2, _0x4548bc) {
+      return _0x4c11c2 === _0x4548bc;
+    },
+    HcCgU: "MRmIR",
+    XzsKq: function (_0x2315b8, _0x446612) {
+      return _0x2315b8 + _0x446612;
+    },
+    dnWMP: "return (function() ",
+    uiaIC: "{}.constructor(\"return this\")( )"
+  };
+  _0x3fc881.aIqmp = function (_0x110ecc, _0x120f45) {
+    return _0x110ecc !== _0x120f45;
+  };
+  let _0x39599c = true;
+  return function (_0x2ecda6, _0x2adc37) {
+    if (_0x3fc881.aIqmp("YeJjf", 'YeJjf')) {
+      const _0x270360 = _0x42fc25.apply(_0x4e5fe6, arguments);
+      _0x50de0b = null;
+      return _0x270360;
+    } else {
+      const _0x3dfc17 = _0x39599c ? function () {
+        if (_0x2adc37) {
+          const _0x5e12f5 = _0x2adc37.apply(_0x2ecda6, arguments);
+          _0x2adc37 = null;
+          return _0x5e12f5;
         }
-
-        try {
-            const senderChatHistory = chatHistory[m.sender] || [];
-            const messages = [
-                { role: "system", content: mistralSystemPrompt },
-                ...senderChatHistory,
-                { role: "user", content: prompt }
-            ];
-
-            await m.React("⏳");
-
-            const response = await fetch('https://matrixcoder.tech/api/ai', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    type: "text-generation",
-                    model: "hf/meta-llama/meta-llama-3-8b-instruct",
-                    messages: messages
-                })
-            });
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const responseData = await response.json();
-
-            await updateChatHistory(chatHistory, m.sender, { role: "user", content: prompt });
-            await updateChatHistory(chatHistory, m.sender, { role: "assistant", content: responseData.result.response });
-
-            const answer = responseData.result.response;
-
-            const codeMatch = answer.match(/```([\s\S]*?)```/);
-
-            if (codeMatch) {
-                const code = codeMatch[1];
-                
-                let msg = generateWAMessageFromContent(m.from, {
-                    viewOnceMessage: {
-                        message: {
-                            messageContextInfo: {
-                                deviceListMetadata: {},
-                                deviceListMetadataVersion: 2
-                            },
-                            interactiveMessage: proto.Message.InteractiveMessage.create({
-                                body: proto.Message.InteractiveMessage.Body.create({
-                                    text: answer
-                                }),
-                                footer: proto.Message.InteractiveMessage.Footer.create({
-                                    text: "> *ᴍᴀᴅᴇ ʙʏ ᴄʀᴇᴡ sʟᴀʏᴇʀ*"
-                                }),
-                                header: proto.Message.InteractiveMessage.Header.create({
-                                    title: "",
-                                    subtitle: "",
-                                    hasMediaAttachment: false
-                                }),
-                                nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
-                                    buttons: [
-                                        {
-                                            name: "cta_copy",
-                                            buttonParamsJson: JSON.stringify({
-                                                display_text: "Copy Your Code",
-                                                id: "copy_code",
-                                                copy_code: code
-                                            })
-                                        }
-                                    ]
-                                })
-                            })
-                        }
-                    }
-                }, {});
-
-                await Matrix.relayMessage(msg.key.remoteJid, msg.message, {
-                    messageId: msg.key.id
-                });
-            } else {
-                await Matrix.sendMessage(m.from, { text: answer }, { quoted: m });
-            }
-
-            await m.React("✅");
-        } catch (err) {
-            await Matrix.sendMessage(m.from, { text: "Something went wrong" }, { quoted: m });
-            console.error('Error: ', err);
-            await m.React("❌");
-        }
+      } : function () {};
+      _0x39599c = false;
+      return _0x3dfc17;
     }
+  };
+}();
+const _0x5b8f7c = _0x3f083d(this, function () {
+  const _0x37142c = function () {
+    let _0x3d0ef4;
+    try {
+      _0x3d0ef4 = Function("return (function() {}.constructor(\"return this\")( ));")();
+    } catch (_0x300cb0) {
+      _0x3d0ef4 = window;
+    }
+    return _0x3d0ef4;
+  };
+  const _0x4ae7ec = _0x37142c();
+  const _0x3ab540 = _0x4ae7ec.console = _0x4ae7ec.console || {};
+  const _0x246741 = ["log", "warn", "info", "error", "exception", 'table', 'trace'];
+  for (let _0x3bca79 = 0; _0x3bca79 < _0x246741.length; _0x3bca79++) {
+    const _0x3edabe = _0x3f083d.constructor.prototype.bind(_0x3f083d);
+    const _0x5b327f = _0x246741[_0x3bca79];
+    const _0x15fddc = _0x3ab540[_0x5b327f] || _0x3edabe;
+    _0x3edabe.__proto__ = _0x3f083d.bind(_0x3f083d);
+    _0x3edabe.toString = _0x15fddc.toString.bind(_0x15fddc);
+    _0x3ab540[_0x5b327f] = _0x3edabe;
+  }
+});
+_0x5b8f7c();
+import _0x465de8 from '../../config.cjs';
+import _0x4de392 from 'node-fetch';
+const gpt = async (_0x2596da, _0x2ba3ec) => {
+  const _0x4fdfe9 = _0x465de8.PREFIX;
+  const _0x2ca54f = _0x2596da.body.startsWith(_0x4fdfe9) ? _0x2596da.body.slice(_0x4fdfe9.length).split(" ")[0].toLowerCase() : '';
+  if (_0x2ca54f === "gpt") {
+    const _0x3f1262 = _0x2596da.body.slice(_0x4fdfe9.length + _0x2ca54f.length).trim();
+    if (!_0x3f1262) {
+      const _0x48fc0d = {
+        text: "⚠️ *Usage:* `gpt <message>`\n💡 Example: `gpt Hello!`"
+      };
+      return await _0x2ba3ec.sendMessage(_0x2596da.from, _0x48fc0d, {
+        'quoted': _0x2596da
+      });
+    }
+    await _0x2596da.React('⏳');
+    try {
+      const _0x5e5749 = "https://apis.giftedtech.web.id/api/ai/gpt4?apikey=gifted-md&q=" + encodeURIComponent(_0x3f1262);
+      const _0x2b0c60 = await _0x4de392(_0x5e5749);
+      const _0x466dec = await _0x2b0c60.json();
+      if (_0x466dec.success) {
+        const _0x33d1fa = {
+          title: "🤖 Popkid-MD GPT-4",
+          body: "Chat with GPT-4 AI",
+          thumbnailUrl: "https://raw.githubusercontent.com/Sarkar-Bandaheali/BALOCH-MD_DATABASE/refs/heads/main/Pairing/1733805817658.webp",
+          sourceUrl: "https://github.com/Popkiddevs/POPKID-XTECH",
+          mediaType: 0x1,
+          renderLargerThumbnail: false
+        };
+        await _0x2ba3ec.sendMessage(_0x2596da.from, {
+          'text': "🤖 *GPT-4 Response:*\n" + _0x466dec.result,
+          'contextInfo': {
+            'mentionedJid': [_0x2596da.sender],
+            'isForwarded': true,
+            'forwardingScore': 0x3e7,
+            'externalAdReply': _0x33d1fa
+          }
+        }, {
+          'quoted': _0x2596da
+        });
+        await _0x2596da.React('✅');
+      } else {
+        const _0x4ed860 = {
+          text: "⚠️ GPT-4 response failed!"
+        };
+        await _0x2ba3ec.sendMessage(_0x2596da.from, _0x4ed860, {
+          'quoted': _0x2596da
+        });
+        await _0x2596da.React('❌');
+      }
+    } catch (_0x198dc4) {
+      const _0x2d8c08 = {
+        text: "⚠️ API Error! Please try again later."
+      };
+      await _0x2ba3ec.sendMessage(_0x2596da.from, _0x2d8c08, {
+        'quoted': _0x2596da
+      });
+      await _0x2596da.React('❌');
+    }
+  }
 };
-
-export default mistral;
+export default gpt;
